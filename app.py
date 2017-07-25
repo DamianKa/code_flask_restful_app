ba19d8f2cfa4625ef8af8d68effd97c400e63cca
DamianKa <damcash@gmail.com>
2017-07-25 21:37:31 +0100

    JWT, authenticate, identity, then tested in Postman

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None) # we can use list or next
         return {'item': item}, 200 if item else 404 # the most popular interview question - 404 - not found.
